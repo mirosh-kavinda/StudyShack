3704866bedfd8f3f8f055826a474ca6678705961
mirosh_kavinda <kavindahgm@gmail.com>
2022-08-30 22:25:13 +0530

    Updates

---
+++ precache-manifest.6150c1501ec01a5b0375fba2f0a9636e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "167eabd65c4f08a1ad4d",
+    "url": "/static/css/main.b2545d1a.chunk.css"
+  },
+  {
+    "revision": "167eabd65c4f08a1ad4d",
+    "url": "/static/js/main.167eabd6.chunk.js"
+  },
+  {
+    "revision": "0d9b695cea0e447c8206",
+    "url": "/static/css/1.49fbc977.chunk.css"
+  },
+  {
+    "revision": "0d9b695cea0e447c8206",
+    "url": "/static/js/1.0d9b695c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "65694c5835ff2300be792219eb54f8a4",
+    "url": "/static/media/G3.65694c58.png"
+  },
+  {
+    "revision": "6e165afda4bbde4f689ffc5f70ed9b98",
+    "url": "/static/media/Rectangle.6e165afd.png"
+  },
+  {
+    "revision": "5c76e43514cb6fe64f9c12027fecb65e",
+    "url": "/static/media/shirley.5c76e435.png"
+  },
+  {
+    "revision": "53c6d8cc1fa20bc9a9788f0cb755d1fc",
+    "url": "/static/media/john.53c6d8cc.png"
+  },
+  {
+    "revision": "21a60ecc365ae9da6af3f9cf98811d35",
+    "url": "/static/media/daniel.21a60ecc.png"
+  },
+  {
+    "revision": "16a4487651ea47e5c88c779a92d9f1b3",
+    "url": "/index.html"
+  }
+];
